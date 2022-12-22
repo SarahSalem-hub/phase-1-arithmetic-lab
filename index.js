c6b45bf6c1e8af9fffaff9bb430a9ce0caa8c6a5
Sarah Al-Dhaferi <55980263+SarahSalem-hub@users.noreply.github.com>
2022-12-22 19:34:08 +0300

    done

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 
 // Write your code here
+let num1 = 31
+let num2 = 2
+let multiply = num1 * num2;
+
+let random = parseInt(Math.random()+1)
+let num3 = 5
+let num4 = 104
+let mod = num4 % num3
+
+let max = Math.max(1,3,4,20)
